84d1c7890a9fffb7c658991c1aac9ee173e2e31f
yoannKy <y.keoxay@gmail.com>
2017-06-21 11:40:34 +0200

    Added series module

---
+++ src/app/series/series.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SeriesRoutingModule } from './series-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SeriesRoutingModule
+  ],
+  declarations: []
+})
+export class SeriesModule { }
