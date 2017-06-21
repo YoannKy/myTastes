11ee63fe70374d79b42f40dfa1dd96d6b0d8c82e
Aberish <=>
2017-06-21 23:17:58 +0200

    add api authentication need save token in local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CallbackHttpService } from './callback-http.service';
 import { AppComponent } from './app.component';
+import { AuthApiComponent } from './auth-api/auth-api.component';
+import { HttpModule }    from '@angular/http';
+import { RouterModule }   from '@angular/router';
+import { RedirectAuthApiComponent } from './redirect-auth-api/redirect-auth-api.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthApiComponent,
+    RedirectAuthApiComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+	  {
+	    path: 'login-api',
+	    component: AuthApiComponent
+	  },
+	  {
+	    path: 'redirect',
+	    component: RedirectAuthApiComponent
+	  },
+	  {
+	    path: '',
+	    redirectTo: '/login-api',
+	    pathMatch: 'full'
+	  }
+    ])
   ],
-  providers: [],
+  providers: [CallbackHttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
