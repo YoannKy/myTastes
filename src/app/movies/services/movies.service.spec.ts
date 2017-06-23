0d99bb8d167bd9fa25e7c49fd93b56c66cc9c85f
yoannKy <y.keoxay@gmail.com>
2017-06-23 09:35:47 +0200

    WIP Unit test

--- src/app/movies/services/movies.service.spec.ts
+++ src/app/movies/services/movies.service.spec.ts
@@ -1,82 +1,67 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {Injectable, ReflectiveInjector} from '@angular/core';
+import { TestBed, tick, fakeAsync } from '@angular/core/testing';
 import {
-  JsonpModule,
-  Jsonp,
   BaseRequestOptions,
-  Response,
+  Http,
   ResponseOptions,
-  Http
+  ConnectionBackend,
+  RequestOptions,
 } from "@angular/http";
-import { MockBackend } from '@angular/http/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Movie } from '../model/movie';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
-  let backend: MockBackend;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [JsonpModule],
-      providers: [
-        MoviesService,
-        MockBackend,
-        BaseRequestOptions,
-        {
-          provide: Jsonp,
-          useFactory: (backend, options) => new Jsonp(backend, options),
-          deps: [MockBackend, BaseRequestOptions]
-        },
-        {
-          provide: Http,
-          useFactory: (backend, options) => new Http(backend, options),
-          deps: [MockBackend, BaseRequestOptions]
-        }
-      ]
-    });
-
-    backend = TestBed.get(MockBackend);
+    this.injector = ReflectiveInjector.resolveAndCreate([
+     {provide: ConnectionBackend, useClass: MockBackend},
+     {provide: RequestOptions, useClass: BaseRequestOptions},
+     Http,
+     MoviesService,
+   ]);
+   this.moviesService = this.injector.get(MoviesService);
+   this.backend = this.injector.get(ConnectionBackend) as MockBackend;
+   this.backend.connections.subscribe((connection: any) => this.lastConnection = connection);
   });
 
   it('should return a PromiseMovie[]>',
-        inject([MoviesService], (moviesService: MoviesService) => {
-
-        const mockResponse = {
-          data: [
-            { id: 0, title: 'Movie 1' },
-            { id: 1, title: 'Movie 2' },
-          ]
-        };
-
-        backend.connections.subscribe((connection) => {
-          connection.mockRespond(new Response(new ResponseOptions({
-            body: JSON.stringify(mockResponse)
-          })));
-        });
-
-        moviesService.getMovies(1).then((movies) => {
-          expect(movies.length).toBe(2);
-          expect(movies[0].title).toEqual('Movie 1');
-          expect(movies[1].title).toEqual('Movie 2');
-        });
-
-    }));
-
-  it('should return a Promise<MovieDetails>'),
-  inject([MoviesService], (moviesService: MoviesService) => {
-
+  fakeAsync(()=> {
+    let movies: Movie[];
     const mockResponse = {
-         id: 0, title: 'Movie 1'
+      movies: [
+        { id: 0, title: 'Movie 1' },
+        { id: 1, title: 'Movie 2' },
+      ]
     };
 
-    backend.connections.subscribe((connection) => {
-      connection.mockRespond(new Response(new ResponseOptions({
-        body: JSON.stringify(mockResponse)
-      })));
-    });
+    this.moviesService.getMovies("").then((movies: Movie[]) => movies = movies);
 
-    moviesService.getMovie(1).then((movie) => {
-      expect(movie.title).toEqual('Movie 1');
-      expect(movie.id).toEqual('0');
-    });
-  });
+    this.lastConnection.mockRespond(new Response(new ResponseOptions({
+        body: JSON.stringify(mockResponse),
+    })));
 
+    tick();
+
+    expect(movies.length).toBe(2);
+    expect(movies[0].title).toEqual('Movie 1');
+    expect(movies[1].title).toEqual('Movie 2');
+  }));
+
+  // it('should return a Promise<MovieDetails>',
+  // fakeAsync(()=> {
+  //   const mockResponse = {
+  //        id: 0, title: 'Movie 1'
+  //   };
+  //
+  //   backend.connections.subscribe((connection) => {
+  //     connection.mockRespond(new Response(new ResponseOptions({
+  //       body: JSON.stringify(mockResponse)
+  //     })));
+  //   });
+  //
+  //   moviesService.getMovie(1).then((movie) => {
+  //     expect(movie.title).toEqual('Movie 1');
+  //     expect(movie.id).toEqual('0');
+  //   });
+  // }));
 });
